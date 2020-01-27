9feed9950605a2d46de7d8b872766697097cfa90
vperezlobato <55490747+vperezlobato@users.noreply.github.com>
2020-01-27 22:11:41 +0100

    Subido

---
+++ Buscaminas/app/src/main/java/com/example/buscaminas/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.buscaminas;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.example.buscaminas.ui.dashboard.DashboardFragment;
+import com.example.buscaminas.ui.home.HomeFragment;
+import com.example.buscaminas.ui.notifications.NotificationsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView ;
+    private ViewPager viewPager;
+    DashboardFragment dashboardFragment;
+    HomeFragment homeFragment;
+    NotificationsFragment notificationsFragment;
+    MenuItem prevMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = findViewById(R.id.nav_view);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.navigation_dashboard:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.navigation_notifications:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment = new HomeFragment();
+        dashboardFragment=new DashboardFragment();
+        notificationsFragment = new NotificationsFragment();
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(dashboardFragment);
+        adapter.addFragment(notificationsFragment);
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ Buscaminas/app/src/main/java/com/example/buscaminas/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.buscaminas.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Fragmento para la clasificacion");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
